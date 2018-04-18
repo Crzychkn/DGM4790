7de7a6399694079ea3a91886ec1d573660adbbde
Arthur Schoenfeld <arthur.schoenfeld@gmail.com>
2018-04-18 20:22:50 +0000

    Added carousel photos and removed categories section.

--- finaldgm4790/resources/views/welcome.blade.php
+++ finaldgm4790/resources/views/welcome.blade.php
@@ -19,10 +19,10 @@
                 <img class="d-block img-fluid" src="{{ asset('storage/rocks1.jpg') }}" alt="First slide">
               </div>
               <div class="carousel-item">
-                <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Second slide">
+                <img class="d-block img-fluid" src="{{ asset('storage/rocks2.jpg') }}" alt="Second slide">
               </div>
               <div class="carousel-item">
-                <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Third slide">
+                <img class="d-block img-fluid" src="{{ asset('storage/rocks3.jpg') }}" alt="Third slide">
               </div>
             </div>
             <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
