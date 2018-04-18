c0b7fdd0d4fc98015ace221e92475f258e70d832
Arthur Schoenfeld <arthur.schoenfeld@gmail.com>
2018-04-18 23:11:34 +0000

    Added login links to nav bar and styled.

--- finaldgm4790/resources/views/layouts/nav.blade.php
+++ finaldgm4790/resources/views/layouts/nav.blade.php
@@ -16,14 +16,38 @@
             <li class="nav-item">
               <a class="nav-link" href="#">Cart</a>
             </li>
-            <li class="nav-item">
-               <a class="nav-link" href="/login">Login</a>
-            </li>
-            <li class="nav-item">
-               <a class="nav-link" href="/register">Register</a>
-            </li>
+            </ul>
 
-          </ul>
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav navbar-right ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Login</a></li>
+                            <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">Register</a></li>
+                        @else
+                            <li class="dropdown nav-item">
+                                <a href="#" class="nav-link" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <a href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            Logout
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                        @endguest
+                    </ul>
+
+
         </div>
       </div>
     </nav>
