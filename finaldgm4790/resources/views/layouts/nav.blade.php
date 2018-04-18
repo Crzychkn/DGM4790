322f277465b7b849562d82ca3bce11dbcb710607
Arthur Schoenfeld <arthur.schoenfeld@gmail.com>
2018-04-18 21:39:14 +0000

    Added basic authentication.

--- finaldgm4790/resources/views/layouts/nav.blade.php
+++ finaldgm4790/resources/views/layouts/nav.blade.php
@@ -17,11 +17,12 @@
               <a class="nav-link" href="#">Cart</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Services</a>
+               <a class="nav-link" href="/login">Login</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Contact</a>
+               <a class="nav-link" href="/register">Register</a>
             </li>
+
           </ul>
         </div>
       </div>
